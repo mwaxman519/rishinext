dc8a9c210bf9b2a7f3c37604933f29bafbb4a936
mwaxman519 <115736601+mwaxman519@users.noreply.github.com>
2025-02-01 21:24:56 +0000

    l;lk

--- server/types.d.ts
+++
@@ -1,8 +0,0 @@
-declare module './vite' {
-  import { Express } from 'express';
-  import { Server } from 'http';
-
-  export function setupVite(app: Express, server: Server): Promise<void>;
-  export function serveStatic(app: Express): void;
-  export function log(message: string, source?: string): void;
-}
